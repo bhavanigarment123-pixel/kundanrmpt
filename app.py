2888d021ed95d227a8e514d72681d5ecd1ec4b1a
bhavanigarment123-pixel <bhavanigarment123@gmail.com>
2026-04-18 14:35:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from scraper import run_scraper
+import subprocess
 
 app = FastAPI()
+
+# 🔥 Install browser at startup (IMPORTANT)
+@app.on_event("startup")
+async def startup_event():
+    print("Installing Playwright browser...")
+    subprocess.run(["playwright", "install", "chromium"])
 
 @app.get("/")
 def home():
