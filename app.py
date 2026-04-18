b83187a1a9d1f278c213a80307d0c2361a5f0e36
bhavanigarment123-pixel <bhavanigarment123@gmail.com>
2026-04-18 15:01:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI
 from scraper import run_scraper
-import subprocess
 
 app = FastAPI()
-
-# 🔥 Install browser at startup (IMPORTANT)
-@app.on_event("startup")
-async def startup_event():
-    print("Installing Playwright browser...")
-    subprocess.run(["playwright", "install", "chromium"])
 
 @app.get("/")
 def home():
@@ -16,6 +9,8 @@ def home():
 
 @app.get("/run")
 async def run():
+    print("🔥 /run endpoint triggered")
+
     try:
         count = await run_scraper()
         return {"scraped_products": count}
