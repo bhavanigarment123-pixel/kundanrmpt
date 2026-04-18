f1d583f86f85bb9dc0aee417295e5a6bfaffb550
bhavanigarment123-pixel <bhavanigarment123@gmail.com>
2026-04-18 15:07:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from scraper import run_scraper
+import subprocess
 
 app = FastAPI()
+
+# 🔥 Install browser at startup (FINAL FIX)
+@app.on_event("startup")
+async def startup_event():
+    print("Installing Chromium...")
+    subprocess.run(["python", "-m", "playwright", "install", "chromium"])
 
 @app.get("/")
 def home():
